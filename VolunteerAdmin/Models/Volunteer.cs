c111b98aa1937df8ff2141f438df79228b4da323
Joey <joey.curtog@gmail.com>
2019-08-18 15:53:18 -0400

    Regex for Volunteer.cs and attempt to display the assignment for each volunteer onto the index page of VolunteerController.cs

--- VolunteerAdmin/Models/Volunteer.cs
+++ VolunteerAdmin/Models/Volunteer.cs
@@ -8,35 +8,46 @@ namespace VolunteerAdmin.Models
         public int ID { get; set; }
 
         [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Za-z]+$")]
         public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [RegularExpression(@"^[A-Za-z]+$")]
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
         [Display(Name = "Home Phone")]
+        [RegularExpression(@"^[0-9]+-[0-9]+-[0-9]+$")]
         public string HomePhone { get; set; }
 
         [Display(Name = "Cell Phone")]
+        [RegularExpression(@"^[0-9]+-[0-9]+-[0-9]+$")]
         public string CellPhone { get; set; }
 
         [Display(Name = "Work Phone")]
+        [RegularExpression(@"^[0-9]+-[0-9]+-[0-9]+$")]
         public string WorkPhone { get; set; }
         public string Address { get; set; }
+
+        [RegularExpression(@"^[A-Za-z]+._*(0-9)?@[a-z]+.[a-z]+$")]
         public string Email { get; set; }
         public string Education { get; set; }
 
         [Display(Name = "Emergency Contact Name")]
+        [RegularExpression(@"^[A-Za-z]+\s+[A-Za-z]+\s+[A-Za-z]+.")]
         public string EmergencyName { get; set; }
 
         [Display(Name = "Emergency Contact Home Phone")]
+        [RegularExpression(@"^[0-9]+-[0-9]+-[0-9]+$")]
         public string EmergencyHomePhone { get; set; }
 
         [Display(Name = "Emergency Contact Work Phone")]
+        [RegularExpression(@"^[0-9]+-[0-9]+-[0-9]+$")]
         public string EmergencyWorkPhone { get; set; }
 
         [Display(Name = "Emergency Contact Email Address")]
+        [RegularExpression(@"^[A-Za-z]+._*(0-9)?@[a-z]+.[a-z]+$")]
         public string EmergencyEmail { get; set; }
 
         [Display(Name = "Emergency Contact Address")]
